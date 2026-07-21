9e19f46b9c321a3817bca05e28ec9d2542b0fa7b
Paul-1783 <paulschmitt144@gmail.com>
2026-07-21 19:58:46 +0200

    made sec fix

--- src/components/Slider/Slider.test.jsx
+++ src/components/Slider/Slider.test.jsx
@@ -1,9 +1,26 @@
 
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+// import userEvent from "@testing-library/user-event";
 import { Slider } from "./Slider";
 
 describe("Slider component", () =>{
-    it("produces all images",() => {})
+    it("produces images",() => {
+        render(<Slider />)
+        expect(screen.getByRole("header-slider").textContent).toMatch(/radical rhinos/i);
+    })
+
+    // it("shows left image after button click", () =>{
+    //     const {container} = render(<Slider />)
+    //     const leftBtn = screen.getByRole("left-btn", { name: "<" });
+
+    //     expect(screen.getByRole("rightBtn").textContent).toMatch(/>/i);
+    // })
+        
+    // it("reaches right image after button ", () =>{
+    //     const {container} = render(<Slider />)
+    //     const rightBtn = screen.getByRole("right-btn", { name: ">" });
+    //     expect(screen.getByRole("rightBtn").textContent).toMatch(/>/i);
+    // })
+
 })
